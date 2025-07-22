46e2543397ddd9a718d71d328f61478defb3d19c
Alexanderjms <jasson.suazo@unah.hn>
2025-07-22 01:01:39 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  @livewireStyles
+</head>
+<body>
+  <h1 class="text-7xl bg-red-500 font-bold underline">
+    Hello world!
+  </h1>
+  
+  @livewireScripts
+</body>
+</html>
