5546a0926834dd0c908e5b2151aec4d72e405438
Alexanderjms <jasson.suazo@unah.hn>
2025-07-27 12:33:20 -0600

    refactor: botons and filters in proyects. new: added tab Estados.new(UI): module projects completed

--- resources/views/components/tabla-crud.blade.php
+++ resources/views/components/tabla-crud.blade.php
@@ -4,11 +4,11 @@
         <h2 class="text-2xl font-bold text-gray-800">
             {{ $titulo }}
         </h2>
-        <div class="flex flex-col sm:flex-row gap-2 flex-1 md:ml-6">
+        <div class="flex flex-col sm:flex-row gap-2 flex-1 md:ml-6 justify-start">
             {{ $filtros }}
         </div>
         @isset($boton)
-        <div>
+        <div class="ml-auto">
             {{ $boton }}
         </div>
         @endisset
