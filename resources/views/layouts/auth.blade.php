149feebfb26547ec70bb9e5654f6c5ebfff49838
Alexanderjms <jasson.suazo@unah.hn>
2025-08-02 16:56:43 -0600

    feat: enhance dashboard with new charts for service orders, quotes, and projects; update layout and styles for better visibility

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -19,7 +19,7 @@
             plugins: []
         }
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     <style>
         input[type="password"]::-ms-reveal,
